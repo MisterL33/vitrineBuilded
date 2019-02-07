dcab9e6bd1d2eaa3d3aa11e783b74694d36ab4bc
code <laurent.julienne@ynov.com>
2019-02-07 19:42:07 +0100

    first version

---
+++ precache-manifest.3df5c350f527415d22d3a77ae37c00ad.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dafa55a87b2b70a23608899908e337e",
+    "url": "/static/media/vague-02.8dafa55a.png"
+  },
+  {
+    "revision": "970864af0b7b06ed3b61",
+    "url": "/static/css/main.be86e033.chunk.css"
+  },
+  {
+    "revision": "aece99ced99aa1b74335",
+    "url": "/static/js/1.aece99ce.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "009f17e6268516e7b9bc3dedbe07ad83",
+    "url": "/static/media/doigt.009f17e6.png"
+  },
+  {
+    "revision": "98fd5a7b23e2095caa0a88263f3734d4",
+    "url": "/static/media/initiale2.98fd5a7b.png"
+  },
+  {
+    "revision": "970864af0b7b06ed3b61",
+    "url": "/static/js/main.970864af.chunk.js"
+  },
+  {
+    "revision": "355007b21ccf5a0a3bce64da2913404c",
+    "url": "/static/media/logo.355007b2.png"
+  },
+  {
+    "revision": "13a41356697e6c3daa0ff9c82bcaddc9",
+    "url": "/static/media/laurent.13a41356.jpg"
+  },
+  {
+    "revision": "60b2ad180f5dae2fba09302aa4a103eb",
+    "url": "/static/media/taupedefensevitrine.60b2ad18.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0b01fc09af1a67160397ebbd676c7b5a",
+    "url": "/static/media/banner.0b01fc09.jpg"
+  },
+  {
+    "revision": "fb322f92a8336e59dc29c8e1ab95f8b9",
+    "url": "/index.html"
+  }
+];
